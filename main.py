5a65f94a2c02727e89933b25e62652227ad2df6f
YuFarhad <113240290+YuFarhad@users.noreply.github.com>
2022-09-23 14:29:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -34,13 +34,25 @@ while it < len(roomsList):
     stateList = []
     while k < lampsAmount:
         stateList.insert(k, tk.IntVar())
-        cbList.insert(k, tk.Checkbutton(text=k+1, variable=stateList[k], onvalue=1, offvalue=0))
-        if k == lampsAmount:
-            checkInRoomslists.insert(it, cbList)
-        cbList[k].grid(row=it*(lampsAmount+1)+k+1, column=0)
         k += 1
+    lampStatusLists.insert(it, stateList)
+    k = 0
+    while k < lampsAmount:
+        cbList.insert(k, tk.Checkbutton(text=k + 1, variable=lampStatusLists[it][k], onvalue=1, offvalue=0))
+        cbList[k].grid(row=it * (lampsAmount + 1) + k + 1, column=0)
+        k += 1
+    checkInRoomslists.insert(it, cbList)
     scaleList.insert(it, tk.Scale(label="Яркость ламп", from_=1, to=100, orient=tk.HORIZONTAL, length=300, width=10))
     scaleList[it].grid(row=it*(lampsAmount+1), column=1)
     it += 1
 
+
+
 f.mainloop()
+
+#check
+
+for i in range(len(lampStatusLists)):
+    for k in range(len(lampStatusLists[i])):
+        print(lampStatusLists[i][k])
+    print()
